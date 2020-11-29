acc8d141c4e6623081e248c2ae80172252075256
Matijs van Zuijlen <matijs@matijs.net>
2020-11-29 11:32:53 +0100

    Define #to_ast on Method and UnboundMethod the regular way
    This resolves EmptyClass offenses and also makes more sense since we're
    opening up the classes anyway to provide documentation. There's a small
    cost in duplication here that could be cleaned up by creating a module
    and including that.

--- lib/live_ast/to_ast.rb
+++ lib/live_ast/to_ast.rb
@@ -2,11 +2,17 @@
 
 require "live_ast/base"
 
-[Method, UnboundMethod].each do |klass|
-  klass.class_eval do
-    def to_ast #:nodoc:
-      LiveAST::Linker.find_method_ast(owner, name, *source_location)
-    end
+class Method
+  # Extract the AST of this object.
+  def to_ast #:nodoc:
+    LiveAST::Linker.find_method_ast(owner, name, *source_location)
+  end
+end
+
+class UnboundMethod
+  # Extract the AST of this object.
+  def to_ast #:nodoc:
+    LiveAST::Linker.find_method_ast(owner, name, *source_location)
   end
 end
 
@@ -15,14 +21,4 @@ class Proc
   def to_ast
     LiveAST::Linker.find_proc_ast(self)
   end
-end
-
-class Method
-  # :method: to_ast
-  # Extract the AST of this object.
-end
-
-class UnboundMethod
-  # :method: to_ast
-  # Extract the AST of this object.
 end
