af1240e56c0ed96dc7aaa5bf597fc1232705a1ba
Matijs van Zuijlen <matijs@matijs.net>
2020-11-29 11:38:46 +0100

    Use modules to provide #to_ast for callables

--- lib/live_ast/to_ast.rb
+++ lib/live_ast/to_ast.rb
@@ -2,23 +2,22 @@
 
 require "live_ast/base"
 
-class Method
-  # Extract the AST of this object.
-  def to_ast #:nodoc:
-    LiveAST::Linker.find_method_ast(owner, name, *source_location)
+module LiveAST
+  module MethodToAST
+    # Extract the AST of this object.
+    def to_ast
+      LiveAST::Linker.find_method_ast(owner, name, *source_location)
+    end
+  end
+
+  module ProcToAST
+    # Extract the AST of this object.
+    def to_ast
+      LiveAST::Linker.find_proc_ast(self)
+    end
   end
 end
 
-class UnboundMethod
-  # Extract the AST of this object.
-  def to_ast #:nodoc:
-    LiveAST::Linker.find_method_ast(owner, name, *source_location)
-  end
-end
-
-class Proc
-  # Extract the AST of this object.
-  def to_ast
-    LiveAST::Linker.find_proc_ast(self)
-  end
-end
+Method.include LiveAST::MethodToAST
+UnboundMethod.include LiveAST::MethodToAST
+Proc.include LiveAST::ProcToAST
