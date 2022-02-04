16a8c2824a2c148d7d2615a8aa5d9cb082c801ea
Matijs van Zuijlen <matijs@matijs.net>
2022-02-04 17:31:49 +0100

    Autocorrect some offenses
    This autocorrects the following offenses:
    
    -  Minitest/AssertIncludes
    -  Minitest/AssertRespondTo
    -  Minitest/AssertTruthy
    -  Minitest/RefuteFalse
    -  Minitest/RefuteIncludes
    -  Minitest/RefuteRespondTo

--- test/to_ast/to_ast_feature_test.rb
+++ test/to_ast/to_ast_feature_test.rb
@@ -7,7 +7,7 @@ require "live_ast/to_ast"
 class ToASTFeatureTest < BaseTest
   def test_require
     [Method, UnboundMethod, Proc].each { |obj|
-      assert obj.instance_methods.include?(:to_ast)
+      assert_includes obj.instance_methods, :to_ast
     }
   end
 end
